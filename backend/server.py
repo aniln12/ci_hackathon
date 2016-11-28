90950a13529c2916358b9aa99112c887c90b5c80
Ryan Pfister <ryan.pfister@parivedasolutions.com>
2016-11-28 14:29:49 -0700

    Implement CORS so front end can access backend

--- backend/server.py
+++ backend/server.py
@@ -1,9 +1,10 @@
 from flask import Flask
+from sqlalchemy import create_engine, text
+from flask.ext.cors import CORS, cross_origin
+
 app = Flask(__name__)
 
-
-from sqlalchemy import create_engine, text
-host, un, pw, port = 'conservation-intl.clqfooklvyn0.us-east-1.rds.amazonaws.com','postgres','postgres',5432 
+host, un, pw, port = 'localhost','postgres','postgres',5432 
 dbname = 'vitalsigns_staging'
 engine = create_engine('postgresql://{un}:{pw}@{host}:{port}/{dbname}'.format(
         un=un,
@@ -12,15 +13,13 @@ engine = create_engine('postgresql://{un}:{pw}@{host}:{port}/{dbname}'.format(
         port=port,
         dbname=dbname))
 
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 sql_text = "SELECT country.name,country.country,agric.landscape_no,landscape.description, landscape.shape FROM public.agric agric, public.country country, public.agric_field_details agric_field_details, public.landscape landscape where agric.country=country.country and agric.country='TZA' and agric_field_details.parent_uuid=agric.uuid and landscape.landscape_no = agric.landscape_no ORDER BY agric_field_details.ag3a_40 desc"
 
 @app.route('/fertilizer_use')
+@cross_origin()
 def fertilizer_use():
 	sql = text(sql_text)
 	result = engine.execute(sql)
