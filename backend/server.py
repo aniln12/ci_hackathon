c7efbe82f417e853c390ad3d21a2bc89970b9ba9
Ryan Pfister <ryan.pfister@parivedasolutions.com>
2016-11-28 16:33:32 -0700

    Fertilizer query working

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from sqlalchemy import create_engine, text
-from flask.ext.cors import CORS, cross_origin
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 
@@ -16,14 +16,17 @@ engine = create_engine('postgresql://{un}:{pw}@{host}:{port}/{dbname}'.format(
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-sql_text = "SELECT country.name,country.country,agric.landscape_no,landscape.description, landscape.shape FROM public.agric agric, public.country country, public.agric_field_details agric_field_details, public.landscape landscape where agric.country=country.country and agric.country='TZA' and agric_field_details.parent_uuid=agric.uuid and landscape.landscape_no = agric.landscape_no ORDER BY agric_field_details.ag3a_40 desc"
+def get_csv(sql_text):
+	sql = text(sql_text)
+	result = engine.execute(sql)
+	header = ",".join(result.keys())
+	rows = "\n".join(",".join(str(v) for v in row) for row in result.fetchall())    
+    
+	return header+"\n"+rows
 
 @app.route('/fertilizer_use')
 @cross_origin()
 def fertilizer_use():
-	sql = text(sql_text)
-	result = engine.execute(sql)
-	header = ",".join(result.keys())
-	rows = "\n".join(",".join(row) for row in result.fetchall())
-	return header+"\n"+rows
+    sql_text = 'SELECT * from public."full_fertilizer_data"'
+    return get_csv(sql_text)
 
