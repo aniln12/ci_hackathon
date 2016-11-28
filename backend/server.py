eee4d28467aba39c3496be9710e57461133cbc7d
Bob Van Zant <bvanzant@brkt.com>
2016-11-28 15:59:11 -0800

    Remove our usage of the cors extension
    Because I can't figure out how to install things via pip and have uwsgi
    actually use them. And this extension package plugin doesn't appear to
    exist in apt. So its basically nonexistent to me.

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from sqlalchemy import create_engine, text
-from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 
@@ -13,20 +12,15 @@ engine = create_engine('postgresql://{un}:{pw}@{host}:{port}/{dbname}'.format(
         port=port,
         dbname=dbname))
 
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
-
 def get_csv(sql_text):
-	sql = text(sql_text)
-	result = engine.execute(sql)
-	header = ",".join(result.keys())
-	rows = "\n".join(",".join(str(v) for v in row) for row in result.fetchall())    
-    
-	return header+"\n"+rows
+    sql = text(sql_text)
+    result = engine.execute(sql)
+    header = ",".join(result.keys())
+    rows = "\n".join(",".join(str(v) for v in row) for row in result.fetchall())
+
+    return header+"\n"+rows
 
 @app.route('/fertilizer_use')
-@cross_origin()
 def fertilizer_use():
     sql_text = 'SELECT * from public."full_fertilizer_data"'
     return get_csv(sql_text)
-
