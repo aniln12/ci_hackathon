3ee5a5520d006cc88d8eace5ca48bcab30fa0771
Ryan Pfister <ryan.pfister@parivedasolutions.com>
2016-11-28 17:32:12 -0700

    Fix server.py issues

--- backend/server.py
+++ backend/server.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from sqlalchemy import create_engine, text
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 
@@ -12,23 +13,27 @@ engine = create_engine('postgresql://{un}:{pw}@{host}:{port}/{dbname}'.format(
         port=port,
         dbname=dbname))
         
-rdbname=r_output
-rengine = create_engine('postgresql://{un}:{pw}@{host}:{port}/{rdbname}'.format(
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+        
+rdbname='r_output'
+rengine = create_engine('postgresql://{un}:{pw}@{host}:{port}/{dbname}'.format(
         un=un,
         pw=pw,
         host=host,
         port=port,
-        dbname=dbname))
+        dbname=rdbname))
 
 def get_csv(sql_text,engine_to_use=engine):
-	sql = text(sql_text)
-	result = engine_to_use.execute(sql)
-	header = ",".join(result.keys())
-    rows = "\n".join(",".join(str(v) for v in row) for row in result.fetchall())
-
-	return header+"\n"+rows
+    sql = text(sql_text)
+    result = engine_to_use.execute(sql)
+    header = ",".join(result.keys())
+    rows = "\n  ".join(",".join(str(v) for v in row) for row in result.fetchall())
+    return header+"\n"+rows
 
 @app.route('/fertilizer_use')
+@cross_origin()
 def fertilizer_use():
     sql_text = 'SELECT * from public."full_fertilizer_data"'
     return get_csv(sql_text)
